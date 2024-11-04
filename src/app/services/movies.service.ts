1e36d0719ae1235c72fa98a56330e9661b528b92
pphan <philipp.phan@adesso.de>
2024-11-04 15:43:47 +0100

    Fixed filter

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable, Signal} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Movie, MovieDetails} from '../model/movie.model';
 import {map, Observable} from 'rxjs';
 
@@ -14,14 +14,16 @@ export class MoviesService {
   }
 
   getMovieDetails(movieId: string): Observable<MovieDetails> {
-    return this.httpClient.get<MovieDetails>('/movies/'+ movieId);
+    return this.httpClient.get<MovieDetails>('/movies/' + movieId);
   }
 
   filterMovieList(title = '', year = ''): Observable<Movie[]> {
     return this.getMovies().pipe(
       map(movies => movies.filter(movie =>
-          (year.length < 4 || year.length === 4 && movie.release_date.split('-')[0].includes(year))
-          && movie.title.toLowerCase().includes(title))
+        (
+          year.length < 4 || year.length === 4 &&
+          movie.release_date.split('-')[0].includes(year))
+        && movie.title.toLowerCase().includes(title.toLowerCase()))
       )
     );
   }
