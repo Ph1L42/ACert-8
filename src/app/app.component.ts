88ecd4e50b2f4c54be7b76d4a54f2f879c975fd1
pphan <philipp.phan@adesso.de>
2024-11-04 11:18:53 +0100

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component, inject, Signal} from '@angular/core';
+import {MovieItemComponent} from './movie-item/movie-item.component';
+import {Movie} from './model/movie.model';
+import {MoviesService} from './services/movies.service';
+import {HighlightDirective} from './highlight.directive';
+import {FavoritesService} from './services/favorites.service';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+
+  templateUrl: 'app.component.html',
+  imports: [
+    MovieItemComponent, HighlightDirective
+  ]
+})
+export class AppComponent {
+}
